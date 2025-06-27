172bf7d6991495635936133a17e4adb29304255f
MicheleDellaquila <micheledellaquila2003@gmail.com>
2025-06-27 11:08:52 +0200

    [TAS-133]: Add the dataValidation middleware for data validation
    Create data validation middleware to ensure data conforms to its schemas.

---
+++ server/middlewares/dataValidation.js
@@ -0,0 +1,18 @@
+const schemas = {};
+
+const dataValidation = async (req, _, next) => {
+  const body = req.body;
+  const schema = schemas[req.path];
+  if (!schema) throw new Error(`No validation schema found for path: ${req.path}`);
+
+  try {
+    const isValidate = await schema.validateAsync(body);
+    if (!isValidate || typeof isValidate !== "object") throw new Error("Validation failed");
+    else next();
+  } catch (error) {
+    console.error("Validation failed:", error.message);
+    throw new Error(`Validation failed: ${error.message}`);
+  }
+};
+
+module.exports = dataValidation;
