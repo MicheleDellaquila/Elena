d8c4b5b928f83e91988545bf007c2ba397469038
MicheleDellaquila <micheledellaquila2003@gmail.com>
2025-07-14 11:35:45 +0200

    [FIX]: Remove control over the course user ID for the enrolled course
    Remove control over the course user ID because the object id regex does that.

--- server/controllers/courses/enrollCourse.js
+++ server/controllers/courses/enrollCourse.js
@@ -1,21 +1,9 @@
-const mongoose = require("mongoose");
 const { AppError } = require("@middleware/errorHandler");
 const enrollmentsModel = require("@models/enrollments");
-const findUserById = require("@models/users");
-const findCourseById = require("@models/courses");
-
-const { ObjectId } = mongoose.Types;
 
 const enrollCourse = async (req, res, next) => {
   try {
     const { courseId, userId } = req.params;
-
-    // Check if course and user exist
-    const user = await findUserById(new ObjectId(userId));
-    if (!user) throw new AppError("Utente non trovato", 404);
-
-    const course = await findCourseById(new ObjectId(courseId));
-    if (!course) throw new AppError("Corso non trovato", 404);
 
     const enrollData = { status: "Da fare", progress: 0, userId, courseId };
     const enrollment = new enrollmentsModel(enrollData);
