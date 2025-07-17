9ba56c497ebe0525f7f6d2aa276ace6aea9d517e
MicheleDellaquila <micheledellaquila2003@gmail.com>
2025-07-17 10:44:32 +0200

    [FIX]: Delete the facet property from the aggregation for wrong use
    Also, rename courseWithTeacher with a better one.

--- server/controllers/courses/getCourses.js
+++ server/controllers/courses/getCourses.js
@@ -7,27 +7,21 @@ const getCourses = async (req, res, next) => {
     const { page, limit } = validatePaginationParams(req.query.page, req.query.limit);
     const offset = calculateOffset(page, limit);
 
-    // Retrieve courses with teacher name
-    const coursesWithTeacher = await coursesModel.aggregate([
-      {
-        $facet: {
-          courses: [
-            { $lookup: { from: "users", localField: "teacher", foreignField: "_id", as: "teacherInfo" } },
-            { $unwind: { path: "$teacherInfo", preserveNullAndEmptyArrays: true } },
-            { $lookup: { from: "enrollments", localField: "_id", foreignField: "courseId", as: "enrollments" } },
-            { $project: { title: 1, description: 1, thumbnail: 1, category: 1, createdAt: 1, teacher: "$teacherInfo.fullName", enrollments: { $size: "$enrollments" } } },
-            { $sort: { createdAt: -1 } },
-            { $skip: offset },
-            { $limit: parseInt(limit) },
-          ],
-        },
-      },
+    // Retrieve courses with teacher name and enrollment count
+    const enrichedCourses = await coursesModel.aggregate([
+      { $lookup: { from: "users", localField: "teacher", foreignField: "_id", as: "teacherInfo" } },
+      { $unwind: { path: "$teacherInfo", preserveNullAndEmptyArrays: true } },
+      { $lookup: { from: "enrollments", localField: "_id", foreignField: "courseId", as: "enrollments" } },
+      { $project: { title: 1, description: 1, thumbnail: 1, category: 1, createdAt: 1, teacher: "$teacherInfo.fullName", enrollments: { $size: "$enrollments" }}},
+      { $sort: { createdAt: -1 } },
+      { $skip: offset },
+      { $limit: parseInt(limit) },
     ]);
 
-    if (!coursesWithTeacher || coursesWithTeacher[0].length === 0) throw new AppError("Nessun corso trovato", 404);
+    if (!enrichedCourses || enrichedCourses.length === 0) throw new AppError("Nessun corso trovato", 404);
 
     const pagination = await getPaginationParams(page, limit);
-    return res.status(200).json({ message: "Corsi recuperati con successo", ...coursesWithTeacher[0], pagination });
+    return res.status(200).json({ message: "Corsi recuperati con successo", courses: enrichedCourses, pagination });
   } catch (error) {
     next(error);
   }
