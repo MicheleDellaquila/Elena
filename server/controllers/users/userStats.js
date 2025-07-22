67596773ca8f7605eda1c451595ed201af1d127c
MicheleDellaquila <micheledellaquila2003@gmail.com>
2025-07-22 11:38:15 +0200

    [FIX]: Rename the user API to userStats to provide a more accurate name of its intended functionality.

--- server/controllers/users/userStats.js
+++ server/controllers/users/userStats.js
@@ -7,27 +7,23 @@ const user = async (req, res, next) => {
   try {
     const userId = recoverUserId(req);
 
-    const userData = await userModel.aggregate([
+    const userStatistics = await userModel.aggregate([
       { $match: { _id: new ObjectId(userId) } },
       { $lookup: { from: "enrollments", localField: "_id", foreignField: "userId", as: "enrollments" } },
       {
         $addFields: {
           stats: {
             enrollments: { $size: "$enrollments" },
-            activeCourses: {
-              $size: { $filter: { input: "$enrollments", cond: { $eq: ["$$this.status", "In corso"] } } },
-            },
-            coursesCompleted: {
-              $size: { $filter: { input: "$enrollments", cond: { $eq: ["$$this.status", "Completato"] } } },
-            },
+            activeCourses: { $size: { $filter: { input: "$enrollments", cond: { $eq: ["$$this.status", "In corso"] } } }},
+            coursesCompleted: { $size: { $filter: { input: "$enrollments", cond: { $eq: ["$$this.status", "Completato"] } } }},
           },
         },
       },
-      { $project: { name: 1, email: 1, role: 1, stats: 1, createdAt: 1 } },
+      { $project: { _id: 0, stats: 1 } },
     ]);
-    if (!userData || userData.length === 0) throw new AppError("Utente non trovato", 404);
+    if (!userStatistics || userStatistics.length === 0) throw new AppError("Utente non trovato", 404);
 
-    res.status(200).json({ data: userData });
+    res.status(200).json({ data: userStatistics });
   } catch (error) {
     next(error);
   }
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
 const verifyUser = require('@middleware/verifyUser');
-const User = require('@controllers/users/user');
+const userStats = require("@controllers/users/userStats");
 const userEnrolledCourses = require('@controllers/users/userEnrolledCourses');
 
 const router = Router();
-router.get('/', verifyUser, User);
+router.get('/', verifyUser, userStats);
 router.get("/enrolled-courses", verifyUser, userEnrolledCourses);
 
 module.exports = router;
