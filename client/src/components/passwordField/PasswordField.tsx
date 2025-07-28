a20352dc8c55d74d0e57458e67c1b51db0d0ba99
MicheleDellaquila <micheledellaquila2003@gmail.com>
2025-07-28 11:40:52 +0200

    [TAS-179]: Enhance the user experience by incorporating the password display logic
    Implement the password display logic to visually display the password that a user has entered.

--- client/src/components/passwordField/PasswordField.tsx
+++ client/src/components/passwordField/PasswordField.tsx
@@ -1,19 +1,42 @@
-import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@components/ui/Form";
-import { Input } from "@components/ui/Input";
 import type { SignUpFormField } from "@/types/components";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@components/ui/Form";
+import { Input } from "@components/ui/Input";
+import { EyeOff, Eye } from "lucide-react";
+import useShowPassword from "./useShowPassword";
 
 const PasswordField = ({ control }: SignUpFormField) => {
+  const { showPassword, toggleShowPassword } = useShowPassword();
+  const inputType = showPassword ? "text" : "password";
+  const inputIcon = showPassword ? <Eye size={18} /> : <EyeOff size={18} />;
+
   return (
     <FormField
       control={control}
       name='password'
       render={({ field }) => (
-        <FormItem className="mb-6">
+        <FormItem className='mb-6'>
           <FormLabel>Password</FormLabel>
-          <FormControl className="relative">
-            <Input placeholder='********' type='password' {...field} />
-          </FormControl>
-          <FormDescription className="text-sm">La password deve contenere almeno 8 caratteri</FormDescription>
+          <div className='relative'>
+            <FormControl className='relative'>
+              <Input placeholder='********' type={inputType} {...field} />
+            </FormControl>
+            <div
+              className='absolute right-3 top-3 cursor-pointer'
+              onClick={toggleShowPassword}
+            >
+              {inputIcon}
+            </div>
+          </div>
+          <FormDescription className='text-sm'>
+            La password deve contenere almeno 8 caratteri
+          </FormDescription>
           <FormMessage />
         </FormItem>
       )}
