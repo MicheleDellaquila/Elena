81122b66572e8b59cca978b76fc1b9969f603096
MicheleDellaquila <micheledellaquila2003@gmail.com>
2025-07-30 12:23:27 +0200

    [FIX]: Include the origin and credentials in the CORS policy
    Configure the origin and credentials to make a request from my client and transmit cookies.

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const loadEnvironmentVariables = () => {
 };
 
 const configureMiddleware = (app) => {
-  app.use(cors());
+  app.use(cors({ origin: process.env.CORS_ORIGIN_DEV, credentials: true }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
