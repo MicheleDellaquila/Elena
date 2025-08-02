52156e71bf4034aa245018b411bf2bb3ae488210
MicheleDellaquila <micheledellaquila2003@gmail.com>
2025-08-02 09:14:57 +0200

    [FIX]:  Remove the check regarding the presence of a refresh or access token, and rectify the decoding token issue because of the property access error.

--- server/controllers/auth/autoLogin.js
+++ server/controllers/auth/autoLogin.js
@@ -1,12 +1,9 @@
-const { AppError } = require("@middleware/errorHandler");
 const { decodeToken } = require("@lib/jwt");
 const findUserById = require("@helpers/findUserById");
 
 const autoLogin = async (req, res, next) => {
   try {
     const accessToken = req.cookies?.accessToken;
-    if (!accessToken) throw new AppError("Non è stato possibile verificare la tua identità", 404);
-
     const decode = decodeToken(accessToken, process.env.ACCESS_TOKEN);
     const user = await findUserById(decode._id);
 
--- server/controllers/auth/refreshAccessToken.js
+++ server/controllers/auth/refreshAccessToken.js
@@ -1,17 +1,14 @@
-const { AppError } = require("@middleware/errorHandler");
 const { generateToken, decodeToken } = require("@lib/jwt");
 const { COMPUTED_COOKIE_OPTIONS, ACCESS_TOKEN_EXPIRY } = require("@helpers/setAuthCookies");
 
 const generateNewAccessToken = (refreshToken) => {
   const decodedToken = decodeToken(refreshToken, process.env.REFRESH_TOKEN);
-  return generateToken({ userId: decodedToken.userId }, process.env.ACCESS_TOKEN, ACCESS_TOKEN_EXPIRY);
+  return generateToken({ userId: decodedToken._id }, process.env.ACCESS_TOKEN, ACCESS_TOKEN_EXPIRY);
 };
 
 const refreshAccessToken = async (req, res, next) => {
   try {
     const refreshToken = req.cookies?.refreshToken;
-    if (!refreshToken) throw new AppError("Refresh token mancante", 400);
-
     const newAccessToken = generateNewAccessToken(refreshToken);
     res.cookie("accessToken", newAccessToken, COMPUTED_COOKIE_OPTIONS);
     res.status(200).json({ ok: true });
